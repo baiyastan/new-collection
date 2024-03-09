d06a624ad469abc54aadd9eb04036ad5ef5b2934
user <jakypjumagulov080gmail.com>
2024-03-09 18:03:47 +0600

    contact beginning

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import "./Contact.css"
+import Banner from '../banner/Banner'
+
 function Contact() {
   return (
-    <div style={{height: "1000px"}}>Contact</div>
+    <div className='contact'>
+      <div className='container'>
+        <Banner />
+      </div>
+      <section className='contact-title'>
+        <div className='container'>
+          <div className='contact-text'>Контактная информация</div>
+          <div className='contact-text-p'>Захотите посмотреть на наше производство - приглашаем в гости. Перед посеващением, обязательно свяжитесь с нами, чтобы согласовать время</div>
+          </div>
+      </section>
+    </div>
   )
 }
 
