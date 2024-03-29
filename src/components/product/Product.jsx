c4342f3edf206f80ece3cb729a32e2b4c9a96c22
user <jakypjumagulov080gmail.com>
2024-03-29 11:45:41 +0600

    box-card is ready

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import ProductName from './ProductName'
-import Card from './Card'
+import ProductName from "./ProductName";
+import Card from "./Card";
 
-import './Product.css'
+import "./Product.css";
+
+const API = "https://fakestoreapi.com/products";
 
 function Product() {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await axios.get(API);
+        setProduct(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProduct();
+  }, []);
   return (
-    <section className='product'>
-      <div className='container'>
-        <div className='product-title'>Какие изделия шьём</div>
-        <div className='product-text'>Специализируемся на мужской, женской и детской одежде 1-2-3 слоя. Шьём всё, кроме вязаных изделий и нежного белья</div>
-        <div className='product-box'>
-            <div className='product-box-name'>
-                <ProductName onClickItem={(name) => console.log(name)} items={['Платья', 'Рубашка', 'Брюки', 'Двойка', 'Футболка', 'Домашная одежда', 'Юбки']}/>
-            </div>
-            <div className='product-box-card'>
-              <Card />
-            </div>
+    <section className="product">
+      <div className="container">
+        <div className="product-title">Какие изделия шьём</div>
+        <div className="product-text">
+          Специализируемся на мужской, женской и детской одежде 1-2-3 слоя. Шьём
+          всё, кроме вязаных изделий и нежного белья
+        </div>
+        <div className="product-box">
+          <div className="product-box-name">
+            <ProductName
+              onClickItem={(name) => console.log(name)}
+              items={[
+                "Платья",
+                "Рубашка",
+                "Брюки",
+                "Двойка",
+                "Футболка",
+                "Домашная одежда",
+                "Юбки",
+              ]}
+            />
+          </div>
+          <div className="product-box-card">
+            <Card items={product}/>
+          </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Product
+export default Product;
