a1c62c34c9a113d1da3f7c885232eb277140ba4d
user <jakypjumagulov080gmail.com>
2024-04-22 19:05:30 +0600

    new contact style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { TfiEmail } from "react-icons/tfi";
-import { FiPhone } from "react-icons/fi";
-import { MdOutlineMessage } from "react-icons/md";
+import EmailSvg from "../../assets/svg/email.svg";
+import MessageSvg from "../../assets/svg/message.svg";
+import PhoneSvg from "../../assets/svg/phone.svg";
 
 import "./Contact.css";
 
@@ -14,22 +14,53 @@ function Contact() {
           <div className="contact-text">Контактная информация</div>
           <div className="contact-item-title">
             <div className="info-contact">
-              <div className="info-text-icon">
-                <FiPhone />
+              <div className="info-contact-icon">
+                <div className="info-text-icon">
+                  <img src={PhoneSvg} alt="svg" />
+                </div>
+                <div className="info-contact-title">Телефон</div>
               </div>
-              <div className="info-text-text">Телефон</div>
+              <div className="contact-text-info display">
+                <strong>
+                  Бишкек:
+                  <a href="tel:9965500992220">+9965 500 992 220</a>
+                </strong>
+                <p>Кыргызская Республика, <br /> г. Бишкек, ул. Дзержинского, 245</p>
+              </div>
             </div>
+            <div className="contact-line"></div>
             <div className="info-contact">
-              <div className="info-text-icon">
-                <MdOutlineMessage/>
+              <div className="info-contact-icon">
+                <div className="info-text-icon">
+                  <img src={MessageSvg} alt="svg" />
+                </div>
+                <div className="info-contact-title">Мессенджеры</div>
               </div>
-              <div className="info-text-text">Мессенджеры</div>
+              <div className="contact-text-info display">
+                <strong>
+                  WhatsApp:{" "}
+                  <a href="https://wa.me/+9965500992220">+9965 500 992 220</a>
+                </strong>
+                <strong>
+                  Telegram:{" "}
+                  <a href="https://t.me/+9965500992220">+9965 500 992 220</a>
+                </strong>
+              </div>
             </div>
-            <div className="info-contact">
-              <div className="info-text-icon">
-                <TfiEmail />
+            <div className="contact-line"></div>
+            <div className="info-contact email">
+              <div className="info-contact-icon">
+                <div className="info-text-icon">
+                  <img src={EmailSvg} alt="svg" />
+                </div>
+                <div className="info-contact-title">Эл. почта</div>
               </div>
-              <div className="info-text-text">Эл. почта</div>
+              <div className="contact-text-info email-style">
+                <p>По всем вопросам пишите нам на почту</p>
+                <a href="molito: imperial.style3@gmail.com">
+                  imperial.style3@gmail.com
+                </a>
+              </div>
             </div>
           </div>
           <div className="contact-item">
