9c21996893ec66d2bee2ea1aecc3ed5e7dcd76df
user <jakypjumagulov080gmail.com>
2024-04-30 17:47:46 +0600

    order is ready

--- src/components/order/index.jsx
+++ src/components/order/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import './Order.css'
 
 function Order() {
   return (
-    <div>
-      gggg
+    <div className="order-container">
+      <div className="container">
+        <h2>Закажите образец</h2>
+        <form className="form">
+          <input type="text" placeholder="Имя" className="name"/>
+          <input type="text" placeholder="Номер телефона" className="phone-number" />
+          <button className="submit">Заказать</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Order
+export default Order;
